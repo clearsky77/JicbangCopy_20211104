827db2f7f8f077898c5ef78cc3a8fd02b8596181
clearsky77 <clearsky3264@gmail.com>
2021-11-04 12:18:18 +0900

    파라미터를 RoomAdapter의 생성자로 받아 -> 부모에게 넘겨줌

--- app/src/main/java/com/clearsky77/jicbangcopy_20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/clearsky77/jicbangcopy_20211104/adapters/RoomAdapter.kt
@@ -1,7 +1,11 @@
 package com.clearsky77.jicbangcopy_20211104.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.clearsky77.jicbangcopy_20211104.datas.RoomData
 
-class RoomAdapter : ArrayAdapter<RoomData>(){
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<RoomData>) : ArrayAdapter<RoomData>(mContext, resId, mList){
 }
