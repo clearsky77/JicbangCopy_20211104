9927045c0315bb2f32b630936101a929f891b363
clearsky77 <clearsky3264@gmail.com>
2021-11-04 13:03:14 +0900

    가격이 1억이 안되는 경우 천단위 표시 - 구글링 활용(kotlin number comma)

--- app/src/main/java/com/clearsky77/jicbangcopy_20211104/adapters/RoomAdapter.kt
+++ app/src/main/java/com/clearsky77/jicbangcopy_20211104/adapters/RoomAdapter.kt
@@ -31,6 +31,7 @@ class RoomAdapter(
         val txtAddress = row.findViewById<TextView>(R.id.txtAddressAndFoolr)
         val txtDescription = row.findViewById<TextView>(R.id.txtDescription)
 
+        txtPrice.text = roomData.getFormattedPrice()
         txtDescription.text = roomData.description
 
         return row
--- app/src/main/java/com/clearsky77/jicbangcopy_20211104/datas/RoomData.kt
+++ app/src/main/java/com/clearsky77/jicbangcopy_20211104/datas/RoomData.kt
@@ -1,9 +1,23 @@
 package com.clearsky77.jicbangcopy_20211104.datas
 
+import java.text.NumberFormat
+import java.util.*
+
 class RoomData(
     val price : Int,
     val address : String,
     val floor : Int,
     val description : String
 ) {
+
+    fun getFormattedPrice() : String {
+        if(this.price < 10000){
+            val priceStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+            return priceStr
+        }else{
+            val priceStr = NumberFormat.getNumberInstance(Locale.KOREA).format(this.price)
+            return priceStr
+        }
+    }
+
 }
