58b75b048311a1f3497dc4857bbefd2e15328bb0
clearsky77 <clearsky3264@gmail.com>
2021-11-04 12:15:02 +0900

    RoomAdapter 추가 + ArrayAdapter 상속  - 기본 생성자 지원 X, 추가 작업 필요

---
+++ app/src/main/java/com/clearsky77/jicbangcopy_20211104/adapters/RoomAdapter.kt
@@ -0,0 +1,7 @@
+package com.clearsky77.jicbangcopy_20211104.adapters
+
+import android.widget.ArrayAdapter
+import com.clearsky77.jicbangcopy_20211104.datas.RoomData
+
+class RoomAdapter : ArrayAdapter<RoomData>(){
+}
--- app/src/main/java/com/clearsky77/jicbangcopy_20211104/datas/RoomData.kt
+++ app/src/main/java/com/clearsky77/jicbangcopy_20211104/datas/RoomData.kt
@@ -1,6 +1,6 @@
 package com.clearsky77.jicbangcopy_20211104.datas
 
-class RooData(
+class RoomData(
     val price : Int,
     val address : String,
     val floor : Int,
